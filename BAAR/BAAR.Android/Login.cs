88ee6a50b6cb574f7857c3064bf4ead5f675258f
Dakota Stickney <dakotastickney@gmail.com>
2017-07-18 09:12:27 -0400

    Authentiation
    YOO

--- BAAR/BAAR.Android/Login.cs
+++ BAAR/BAAR.Android/Login.cs
@@ -10,6 +10,7 @@ using Android.OS;
 using Android.Runtime;
 using Android.Views;
 using Android.Widget;
+using Xamarin.Auth;
 
 namespace BAAR.Droid
 {
@@ -24,9 +25,38 @@ namespace BAAR.Droid
 
             ImageButton button = FindViewById<ImageButton>(Resource.Id.DebugButton);
             button.Click +=  (sender, e) => {
+               // var activity = this.BaseContext as Activity;
+                // Get our button from the layout resource,
+                // and attach an event to it
+                // var Test = new OAuth2Request("GET");
+                var authenticator = new OAuth2Authenticator(
+                      "679b09c5-2498-45e3-a6b4-929516a7d732",
+                      "read",
+                      new Uri("https://www.facebook.com"),
+                      new Uri("https://www.google.com"));
+                authenticator.Completed += (sender1, args) =>
+                {
+                    Console.WriteLine("This is definately working!!");
+                    if (args.IsAuthenticated)
+                    {
+                        var account = args.Account;
+                        AlertDialog.Builder builder1 = new AlertDialog.Builder(this);
+                        builder1.SetMessage(args.Account.Username);
+                        AlertDialog alert11 = builder1.Create();
+                        alert11.Show();
+                        Console.WriteLine(account.Username);
+                        // Do success work 
+                    }
+                    else
+                    {
+                        // The user cancelled 
 
-                var NewScreen = new Intent(this, typeof(MainActivity));
-                StartActivity(NewScreen);
+                    }
+                };
+                    Console.Write("This is definately Not Working");
+                    this.StartActivity(authenticator.GetUI(this));
+              //  var NewScreen = new Intent(this, typeof(MainActivity));
+               // StartActivity(NewScreen);
             };
 
         }
--- BAAR/BAAR.Android/MainActivity.cs
+++ BAAR/BAAR.Android/MainActivity.cs
@@ -8,45 +8,42 @@ using Android.Content.PM;
 using Android.OS;
 using ZXing.Mobile;
 using Xamarin.Auth;
+using System.Threading;
+using System.Threading.Tasks;
 
 namespace BAAR.Droid
 {
     [Activity(Label = "BarcodeScanner", ScreenOrientation = ScreenOrientation.Portrait)]
     public class MainActivity : Activity
-	{
-		protected override void OnCreate (Bundle bundle)
-		{
-			base.OnCreate (bundle);
+    {
+        protected override void OnCreate(Bundle bundle)
+        {
+            base.OnCreate(bundle);
             Window.RequestFeature(WindowFeatures.NoTitle);
             // Set our view from the "main" layout resource
             SetContentView(Resource.Layout.Main);
-
-			// Get our button from the layout resource,
-			// and attach an event to it
-			ImageButton button = FindViewById<ImageButton> (Resource.Id.scanButton);
-
-          /* var authenticator = new OAuth2Authenticator(
-                clientId,
-                null,
-                Constants.Scope,
-                new Uri(Constants.AuthorizeUrl),
-                new Uri(redirectUri),
-                new Uri(Constants.AccessTokenUrl),
-                null,
-                true);*/
-
             MobileBarcodeScanner.Initialize(Application);
-            button.Click += async (sender, e) => {
+            ImageButton button = FindViewById<ImageButton>(Resource.Id.scanButton);
+            button.Click += async (sender, e) =>
+            {
 
                 var scanner = new ZXing.Mobile.MobileBarcodeScanner();
                 var result = await scanner.Scan();
 
-                var NewScreen = new Intent(this,typeof(studentac));
+                var NewScreen = new Intent(this, typeof(studentac));
                 NewScreen.PutExtra("StudentID", result.Text);
                 StartActivity(NewScreen);
             };
+          
+           }
+
+    public class Test : FormAuthenticator
+        {
+            public override Task<Account> SignInAsync(CancellationToken cancellationToken)
+            {
+                throw new NotImplementedException();
+            }
         }
-	}
-}
+    } }
 
 
