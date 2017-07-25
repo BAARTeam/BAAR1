d07542881f7bd0bb7bea4846da280e59b6affe61
Jacob Salinas <soccersalinas18@gmail.com>
2017-07-25 08:20:41 -0400

    REST API Call
    This clip uses login activity to send a REST API Call and get the access token with the client ID and secret

--- BAAR/BAAR.Android/Login.cs
+++ BAAR/BAAR.Android/Login.cs
@@ -11,6 +11,11 @@ using Android.Runtime;
 using Android.Views;
 using Android.Widget;
 using Xamarin.Auth;
+using System.Net.Http;
+using System.Net.Http.Headers;
+using System.Threading.Tasks;
+using System.Net;
+using System.IO;
 
 namespace BAAR.Droid
 {
@@ -25,41 +30,75 @@ namespace BAAR.Droid
 
             ImageButton button = FindViewById<ImageButton>(Resource.Id.DebugButton);
             button.Click +=  (sender1, e) => {
-               // var activity = this.BaseContext as Activity;
-                // Get our button from the layout resource,
-                // and attach an event to it
-                // var Test = new OAuth2Request("GET");
-                    Console.WriteLine("This is definately working!!");
-                UriBuilder Thing = new UriBuilder("172.21.123.196/public");
-                var authenticator = new OAuth2Authenticator(
-                      "679b09c5-2498-45e3-a6b4-929516a7d732",
-                      "read",
-                      Thing.Uri,
-                      new Uri("http://172.21.123.196/guardian/bulletin2.html"));
+                /* // var activity = this.BaseContext as Activity;
+                  // Get our button from the layout resource,
+                  // and attach an event to it
+                  // var Test = new OAuth2Request("GET");
+                      Console.WriteLine("This is definately working!!");
+                  UriBuilder Thing = new UriBuilder("172.21.123.196/public");
+                  var authenticator = new OAuth2Authenticator(
+                        "679b09c5-2498-45e3-a6b4-929516a7d732",
+                        "read",
+                        Thing.Uri,
+                        new Uri("http://172.21.123.196/guardian/bulletin2.html"));
 
-             
-                authenticator.Completed += (sender, args) =>
+
+                  authenticator.Completed += (sender, args) =>
+                  {
+                      if (args.IsAuthenticated)
+                      {
+                          var account = args.Account;
+                          Console.WriteLine("YOooooo");
+
+                          // Do success work 
+                      }
+                      else
+                      {
+                          // The user cancelled 
+                          Console.WriteLine("YOooooo");
+                      }
+                  };
+                  authenticator.OnError("Blasphemy");
+                      Console.Write("This is definately Not Working");
+                      this.StartActivity(authenticator.GetUI(this));*/
+
+
+                //var rxcui = "grant_type=client_credentials";
+                //youre trying to pass the body as seen above but cant figure out how to get it in the request.
+                //perhaps you need to just put in in the url
+
+                var request = HttpWebRequest.Create(string.Format(@"http://172.21.123.196/oauth/access_token?{0}", "grant_type=client_credentials"));
+                request.ContentType = "application/x-www-form-urlencoded;charset=UTF-8";
+                request.Method = "POST";
+                request.Headers.Add(HttpRequestHeader.Authorization,"Basic MTM2ZDZmNzEtYTYzOS00Nzc1LWIxMjktNDMwNWE4YjA4ZDZkOjQzNDFmMjNmLTczZTEtNGI4ZS1iMzNjLTVhMWQ5MTkyZDczNQ==");
+                
+                
+                
+
+                using (HttpWebResponse response = request.GetResponse() as HttpWebResponse)
                 {
-                    if (args.IsAuthenticated)
+                    if (response.StatusCode != HttpStatusCode.OK)
+                        Console.Out.WriteLine("Error fetching data. Server returned status code: {0}", response.StatusCode);
+                    using (StreamReader reader = new StreamReader(response.GetResponseStream()))
                     {
-                        var account = args.Account;
-                        Console.WriteLine("YOooooo");
+                        var content = reader.ReadToEnd();
+                        if (string.IsNullOrWhiteSpace(content))
+                        {
+                            Console.Out.WriteLine("Response contained empty body...");
+                        }
+                        else
+                        {
+                            Console.Out.WriteLine("Response Body: \r\n {0}", content);
+                        }
 
-                        // Do success work 
+                        //Assert.NotNull(content);
                     }
-                    else
-                    {
-                        // The user cancelled 
-                        Console.WriteLine("YOooooo");
-                    }
-                };
-                authenticator.OnError("Blasphemy");
-                    Console.Write("This is definately Not Working");
-                    this.StartActivity(authenticator.GetUI(this));
-                //Intent Testing = new Intent(this, typeof(MainActivity));
-                //StartActivity(Testing);
-                //  var NewScreen = new Intent(this, typeof(MainActivity));
-                // StartActivity(NewScreen);
+                }
+
+                Intent Testing = new Intent(this, typeof(MainActivity));
+                StartActivity(Testing);
+                  var NewScreen = new Intent(this, typeof(MainActivity));
+                 StartActivity(NewScreen);
             };
 
         }
