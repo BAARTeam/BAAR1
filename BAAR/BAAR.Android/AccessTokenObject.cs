6f51f86fd4323ade70925d8b10b71b1abce27151
Dakota Stickney <dakotastickney@gmail.com>
2017-07-26 09:55:15 -0400

    FIxes
    Broke Some Stuff

---
+++ BAAR/BAAR.Android/AccessTokenObject.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Android.App;
+using Android.Content;
+using Android.OS;
+using Android.Runtime;
+using Android.Views;
+using Android.Widget;
+using Newtonsoft.Json;
+
+namespace BAAR.Droid
+{
+    public class AccessObject
+    {
+        [JsonProperty("access_token")]
+        public string AccessToken;
+        [JsonProperty("token_type")]
+        public string Bearer;
+        [JsonProperty("expires_in")]
+        public string Expiration;
+    }
+}
