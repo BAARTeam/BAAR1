02e37d17c895761b2f40cfde74e5d72107ccc1e2
Dakota Stickney <dakotastickney@gmail.com>
2017-08-18 09:57:05 -0400

    Padding Change

--- BAAR/BAAR.Android/MainActivity.cs
+++ BAAR/BAAR.Android/MainActivity.cs
@@ -41,11 +41,12 @@ namespace BAAR.Droid
                 button.ScaleX = ((float)(.9));
                 button.ScaleY = ((float)(.9));
 
+                button.ScaleX = ((float)(10/9));
+                button.ScaleY = ((float)(10/9));
+
                 //opens the studentac page with tickets
                 var NewScreen = new Intent(this, typeof(studentac));
                 StartActivity(NewScreen);
-                button.ScaleX = ((float)(10/9));
-                button.ScaleY = ((float)(10/9));
             };
 
         }
